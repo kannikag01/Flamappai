29a6685d3b85e9db1ccc4440e7352131a495011b
kannikag01 <kannikagiri01@.com>
2025-11-14 00:33:32 -0800

    Added OpenCV initialization in MainActivity

--- app/src/main/java/com/flamapp/MainActivity.java
+++ app/src/main/java/com/flamapp/MainActivity.java
@@ -1,24 +1,31 @@
 package com.flamapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Load the native library
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    // Declare JNI function
+    public native String stringFromJNI();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Initialize OpenCV
+        OpenCVLoader.initDebug();
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 }
